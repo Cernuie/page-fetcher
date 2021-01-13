e31f98261e44ae752ca66272717b31d695a3615f
Alexander Nguyen <cernvii@gmail.com>
2021-01-13 23:51:09 +0000

    finished rest of base cases

--- fetcher.js
+++ fetcher.js
@@ -1,32 +1,44 @@
-args = process.argv.slice(2);
+let args = process.argv.slice(2);
 const request = require('request');
 const fs = require('fs');
 let url = args[0];
 let filePath = args[1];
 
 const req = (url,filePath) => {
+  //uses fs access to check if file exists
   fs.access(filePath, fs.F_OK, (err) => {
     if (err) {
-      console.error(err)
+      console.error(err);
       return console.log("File does not exist");
-    }
-  
-  request(url, (error, response, body) => {
-  fs.writeFile(filePath, body, 'utf8', (err) => {
-    if(err){
-      throw err;
-    } else{
-      fs.stat(filePath, (err, stats) => {
-        if (err) {
-          throw err;
-        } else {
-          console.log(`Downloaded and saved ${stats.size} bytes to ${filePath}`);
+    } else {
+      //if it does then it'll write to it using request
+      request(url, (error, response, body) => {
+        //checks if request returns an error
+        if (error) {
+          console.log("We got an error! Please try again.");
         }
-      })
+        // and if status code isn't equal to 200
+        if (response.statusCode !== 200) {
+          console.log(`status code: ${response} ${response.statusCode}, something is wrong, try again!`);
+        }
+        // then writes the file to the specified file
+        fs.writeFile(filePath, body, 'utf8', (err) => {
+          if (err) {
+            throw err;
+          } else {
+            //uses fs stat to find the file size.
+            fs.stat(filePath, (err, stats) => {
+              if (err) {
+                throw err;
+              } else {
+                console.log(`Downloaded and saved ${stats.size} bytes to ${filePath}`);
+              }
+            });
+          }
+        });
+      });
     }
-  })
-  })
-})
-}
+  });
+};
 
 req(url,filePath);
